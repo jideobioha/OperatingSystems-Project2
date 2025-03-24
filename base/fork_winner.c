5676819fc0b55c15eeb946739913074f4e539d53
Jidechukwu Obioha <jobioha1@cs.binghamton.edu>
2025-03-24 16:30:31 -0400

    started fork winner system call stuff

---
+++ base/fork_winner.c
@@ -0,0 +1,7 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+
+// this file calls the system call that allows us to choose
+// whether 
--- base/user.h
+++ base/user.h
@@ -23,6 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+void fork_winner(int);
 
 // ulib.c
 int stat(char*, struct stat*);
