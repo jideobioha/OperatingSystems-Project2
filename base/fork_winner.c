fa0cfd4d93f6186751163484d8adafd26faa89b3
Jidechukwu Obioha <jobioha1@cs.binghamton.edu>
2025-03-24 23:17:30 -0400

    Finished fork_winner system call with an error i will fix tomorrow

--- base/fork_winner.c
+++ base/fork_winner.c
@@ -4,4 +4,30 @@
 
 
 // this file calls the system call that allows us to choose
-// whether 
+// whether we want the scheduler to run with the parent-first
+// or child-first policy
+
+
+int
+main(int argc, char* argv[]){
+   
+    if (argc != 2){
+        printf(1, "Usage: %s 0|1"
+		  "\t0: Parent is scheduled to run most often\n"	
+		  "\t1: Child is scheduled to run most often\n", argv[0]);
+	exit();
+    }
+   
+    int i = atoi(argv[1]);
+    
+    if(i != 1 || i != 0){
+        printf(1, "Please provide the correct arguments.\n");
+	exit();
+    }    
+
+    fork_winner(i); // passing arg to user space wrapper
+		    // wrapper is void fork_winner(int winner)
+
+
+   exit(); // return 0;
+}
